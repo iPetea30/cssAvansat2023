9ef41f5adf324877d55ab1bd9c6f65b6b7055271
iPetea30 <dumi.puia@gmail.com>
2023-02-15 22:38:45 +0200

    curs5

---
+++ acasa/js/exercitii.js
@@ -0,0 +1,118 @@
+const country = 'Romania';
+const continent = 'Europa';
+let population = 18;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+
+
+// let myName = 'Numele meu este Petea';
+// let age = 33;
+// console.log(myName);
+// console.log(age);
+
+// let PI = 3.14;
+// let Person = 'person';
+
+let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// javaScriptIsFun = false;
+// console.log(javaScriptIsFun);
+
+// console.log(typeof javaScriptIsFun);
+// javaScriptIsFun = 23;
+// console.log(typeof javaScriptIsFun);
+// javaScriptIsFun = '23';
+// console.log(typeof javaScriptIsFun);
+
+
+//// tipuri de date
+// let year;
+// console.log(year);
+// console.log(typeof undefined);
+
+// year = 1989;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let island = false;
+const language = 'romana';
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+
+//// let, const, var
+// let age = 30;
+// age = 31;
+// console.log(age);
+
+// const birthYear = 1991;
+// console.log(birthYear);
+
+// var myAge = 20;
+// myAge = 22;
+// console.log(myAge);
+
+// let myAge2 = 30;
+// console.log(myAge2);
+
+// language = 'portuguese';
+
+
+//// operatorii de baza
+// let now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2021;
+
+// // console.log(ageJonas);
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // 2 ** 3 = 2 la puterea a 3 a. 2 * 2 * 2 = 8.
+
+// const firstName = 'Ion';
+// const lastName = 'Ciobanu';
+// console.log(firstName);
+// console.log(lastName);
+// console.log(firstName + lastName + firstName + firstName + lastName); // + reprezinta concatenare - impreunare = IonCiobanuIonIonCiobanu
+// console.log(firstName + " " + lastName); //Ion Ciobanu
+
+// //Assigment operators
+// let x = 10 + 5; // 15
+// x+= 10; // x = x + 10 // 25
+// console.log(x); //25
+// x -= 5; // x = x - 5 // 20
+// console.log(x); // 20
+// x *= 5; // x = x * 5 // 100
+// console.log(x); // 100
+
+// x++ // x = x + 1
+// x--;
+// x--; 
+// console.log(x); // 99
+
+// //Comparison operators
+// console.log(10 > 8); //false
+// console.log(10 < 8); // true
+// console.log(ageJonas > ageSarah); // true
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(isFullAge); // false // 16
+
+// console.log(now - 1991 > now - 2021); // true // 
+
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// const description1 = country + ' is in ' + continent + ' and it`s ' + population + ' milion people speaks ' + language;
+// console.log(description1);
+
+
